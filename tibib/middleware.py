d2b92c86da2bd6f16840dc67405089f997eac21b
KenKomKom <kenichikomala@gmail.com>
2025-03-25 20:32:48 +0700

    merge with refactor-bodhi

--- tibib/middleware.py
+++ tibib/middleware.py
@@ -10,7 +10,6 @@ class JWTAuthenticationMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        # Mastiin url ini bisa di akses tanpa jwt token
         exempt_paths = [
             '/login/', 
             '/register/', 
@@ -22,12 +21,17 @@ class JWTAuthenticationMiddleware:
         current_path = request.path
         if any(current_path.startswith(path) for path in exempt_paths):
             return self.get_response(request)
-
+        
+        print("ini middle ware")
         auth_header = request.headers.get("Authorization")
+        auth_cookie = request.COOKIES.get("jwt_token")
 
-        token = None
+        if auth_header and not auth_header:
+            return JsonResponse({"error": "Authorization header missing"}, status=401)
 
+        # dari postman
         if auth_header and auth_header.startswith("Bearer "):
+            print("auth_header",auth_header)
             token = auth_header.split(" ")[1]
             try:
                 payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=["HS256"])
@@ -45,10 +49,40 @@ class JWTAuthenticationMiddleware:
                 
             except jwt.InvalidTokenError as e:
                 print("JWT Error:", str(e))
-                if request.path.startswith('/api/'):
-                    return JsonResponse({"error": "Invalid token"}, status=401)
-                else:
-                    # For non-API routes, redirect to login
-                    return redirect('login')
-                
+                return JsonResponse({"error": "Invalid token"}, status=401)
+        # ini kalo dari frontend
+        elif auth_cookie:
+            token = auth_cookie
+            token = auth_cookie.split(" ")[1]
+            try:
+                payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=["HS256"])
+                request.user_id = payload["id"]  # Attach user ID to request
+                request.user_username = payload['full_phone']
+                request.user_role = payload['role']
+            except jwt.ExpiredSignatureError:
+                return JsonResponse({"error": "cookie has expired"}, status=401)
+            except jwt.InvalidTokenError as e:
+                print("JWT Error:", str(e))
+                return JsonResponse({"error": "Invalid cookie"}, status=401)
+
+        return self.get_response(request)
+
+class JWTBlacklistMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        auth_header = request.headers.get("Authorization")
+        if auth_header and auth_header.startswith("Bearer "):
+            token = auth_header.split(" ")[1]
+            if BlacklistedToken.objects.filter(token=token).exists():
+                return JsonResponse({"error": "Token is blacklisted"}, status=401)
+
+            try:
+                jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+            except jwt.ExpiredSignatureError:
+                return JsonResponse({"error": "Token has expired"}, status=401)
+            except jwt.InvalidTokenError:
+                return JsonResponse({"error": "Invalid token"}, status=401)
+
         return self.get_response(request)
